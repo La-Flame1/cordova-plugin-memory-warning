a7d583f01039128579f141d40f388f2e0f751bb9
Donell Mario Oageng <donell.oageng15@gmail.com>
2026-08-07 13:17:10 +0200

    Return only camera memory safety boolean

--- src/android/CordovaPluginMemoryWarning.java
+++ src/android/CordovaPluginMemoryWarning.java
@@ -6,17 +6,15 @@ import android.content.Context;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class CordovaPluginMemoryWarning extends CordovaPlugin {
-    private static final String ACTION_GET_CAMERA_MEMORY_STATUS = "getCameraMemoryStatus";
+    private static final String ACTION_IS_SAFE_TO_OPEN_CAMERA = "isSafeToOpenCamera";
     private static final int DEFAULT_CAMERA_RESERVE_MB = 128;
     private static final long BYTES_PER_MB = 1024L * 1024L;
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
-        if (!ACTION_GET_CAMERA_MEMORY_STATUS.equals(action)) {
+        if (!ACTION_IS_SAFE_TO_OPEN_CAMERA.equals(action)) {
             return false;
         }
 
@@ -24,9 +22,9 @@ public class CordovaPluginMemoryWarning extends CordovaPlugin {
             @Override
             public void run() {
                 try {
-                    callbackContext.success(getCameraMemoryStatus());
+                    callbackContext.success(isSafeToOpenCamera() ? 1 : 0);
                 } catch (Exception exception) {
-                    callbackContext.error("Unable to read device memory: " + exception.getMessage());
+                    callbackContext.error("Unable to check camera memory safety: " + exception.getMessage());
                 }
             }
         });
@@ -34,7 +32,7 @@ public class CordovaPluginMemoryWarning extends CordovaPlugin {
         return true;
     }
 
-    private JSONObject getCameraMemoryStatus() throws JSONException {
+    private boolean isSafeToOpenCamera() {
         Context context = cordova.getContext();
         ActivityManager activityManager =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
@@ -54,18 +52,8 @@ public class CordovaPluginMemoryWarning extends CordovaPlugin {
             cameraReserveMB = DEFAULT_CAMERA_RESERVE_MB;
         }
 
-        long cameraReserveBytes = cameraReserveMB * BYTES_PER_MB;
         long headroomBytes = memoryInfo.availMem - memoryInfo.threshold;
-        boolean safeToOpenCamera =
-                !memoryInfo.lowMemory && headroomBytes >= cameraReserveBytes;
-
-        JSONObject status = new JSONObject();
-        status.put("lowMemory", memoryInfo.lowMemory);
-        status.put("availableMemMB", memoryInfo.availMem / BYTES_PER_MB);
-        status.put("thresholdMB", memoryInfo.threshold / BYTES_PER_MB);
-        status.put("headroomMB", headroomBytes / BYTES_PER_MB);
-        status.put("cameraReserveMB", cameraReserveMB);
-        status.put("safeToOpenCamera", safeToOpenCamera);
-        return status;
+        long cameraReserveBytes = cameraReserveMB * BYTES_PER_MB;
+        return !memoryInfo.lowMemory && headroomBytes >= cameraReserveBytes;
     }
 }
