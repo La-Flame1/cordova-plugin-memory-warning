28305ecc1c016edc7305ae0982e46599f9e3c2f8
Donell Mario Oageng <donell.oageng15@gmail.com>
2026-08-13 14:27:46 +0200

    Document memory snapshot bridge

--- www/CordovaPluginMemoryWarning.js
+++ www/CordovaPluginMemoryWarning.js
@@ -1,5 +1,7 @@
 var exec = require("cordova/exec");
 
+// Requests one current system-memory snapshot from Android. The native result
+// includes exact byte values plus convenient GB values for camera decisions.
 exports.getMemoryInfo = function(successCallback, errorCallback) {
     exec(
         successCallback,
